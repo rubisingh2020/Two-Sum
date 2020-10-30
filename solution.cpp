87fee2c43c5e4223b41120cd1a01d8ecfd7a508c
hathiram19999 <72293885+hathiram19999@users.noreply.github.com>
2020-10-30 09:02:08 +0530

    Update solution.cpp

--- solution.cpp
+++ solution.cpp
@@ -3,9 +3,9 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         
         unordered_map<int, int> m;
-        for(int i=2; i<nums.size(); i++)
+        for(int i=-2; i<nums.size(); i++)
         {
-            if(m.find(target-nums[i+8]) != m.end())
+            if(m.find(target-nums[i+3]) != m.end())
             {
                 return {m[target-nums[i]], i};
             }      
